eaee5f561f16a6bb3fde10ac03fcc9dad83a7429
archive-user <www.766111444@foxmail.com>
2025-05-26 00:27:12 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:desktop_webview_window/desktop_webview_window.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -32,32 +35,64 @@ class _WebViewExampleState extends State<WebViewExample> {
   late final WebViewController _controller;
 
   @override
-  void initState() {
+  void initState() async {
     super.initState();
 
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (String url) async {
-            // 页面加载完成后获取网页内容
-            try {
-              final String? pageContent =
-                  await _controller.runJavaScriptReturningResult(
-                      'document.documentElement.outerHTML;') as String?;
+    if (Platform.isLinux) {
+      final webview = await WebviewWindow.create();
+      webview
+        ..registerJavaScriptMessageHandler("test", (name, body) {
+          debugPrint('on javaScipt message: $name $body');
+        })
+        ..setApplicationNameForUserAgent(" WebviewExample/1.0.0")
+        ..setPromptHandler((prompt, defaultText) {
+          if (prompt == "test") {
+            return "Hello World!";
+          } else if (prompt == "init") {
+            return "initial prompt";
+          }
+          return "";
+        })
+        ..addScriptToExecuteOnDocumentCreated("""
+  const mixinContext = {
+    platform: 'Desktop',
+    conversation_id: 'conversationId',
+    immersive: false,
+    app_version: '1.0.0',
+    appearance: 'dark',
+  }
+  window.MixinContext = {
+    getContext: function() {
+      return JSON.stringify(mixinContext)
+    }
+  }
+""")
+        ..launch("https://flutter.dev/");
+    } else {
+      _controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setNavigationDelegate(
+          NavigationDelegate(
+            onPageFinished: (String url) async {
+              // 页面加载完成后获取网页内容
+              try {
+                final String? pageContent =
+                    await _controller.runJavaScriptReturningResult(
+                        'document.documentElement.outerHTML;') as String?;
 
-              if (pageContent != null) {
-                print('网页内容: $pageContent');
-              } else {
-                print('未能获取网页内容');
+                if (pageContent != null) {
+                  print('网页内容: $pageContent');
+                } else {
+                  print('未能获取网页内容');
+                }
+              } catch (e) {
+                print('获取网页内容时出错: $e');
               }
-            } catch (e) {
-              print('获取网页内容时出错: $e');
-            }
-          },
-        ),
-      )
-      ..loadRequest(Uri.parse('https://flutter.dev/'));
+            },
+          ),
+        )
+        ..loadRequest(Uri.parse('https://flutter.dev/'));
+    }
   }
 
   @override
