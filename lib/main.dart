c10f651938e2572af2de682efcd2a1e91417044a
archive-user <www.766111444@foxmail.com>
2025-05-26 00:41:38 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,58 +2,78 @@ import 'dart:io';
 
 import 'package:desktop_webview_window/desktop_webview_window.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
+void main(List<String> args) {
+  debugPrint('args: $args');
+  if (runWebViewTitleBarWidget(args)) {
+    return;
+  }
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController(
+    text: 'https://example.com',
+  );
+
+  bool? _webviewAvailable;
+
+  @override
+  void initState() {
+    super.initState();
+    WebviewWindow.isWebviewAvailable().then((value) {
+      setState(() {
+        _webviewAvailable = value;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    print('is OK!!');
     return MaterialApp(
-      title: 'WebView Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const WebViewExample(),
-    );
-  }
-}
-
-class WebViewExample extends StatefulWidget {
-  const WebViewExample({super.key});
-
-  @override
-  State<WebViewExample> createState() => _WebViewExampleState();
-}
-
-class _WebViewExampleState extends State<WebViewExample> {
-  late final WebViewController _controller;
-
-  @override
-  void initState() async {
-    super.initState();
-
-    if (Platform.isLinux) {
-      final webview = await WebviewWindow.create();
-      webview
-        ..registerJavaScriptMessageHandler("test", (name, body) {
-          debugPrint('on javaScipt message: $name $body');
-        })
-        ..setApplicationNameForUserAgent(" WebviewExample/1.0.0")
-        ..setPromptHandler((prompt, defaultText) {
-          if (prompt == "test") {
-            return "Hello World!";
-          } else if (prompt == "init") {
-            return "initial prompt";
-          }
-          return "";
-        })
-        ..addScriptToExecuteOnDocumentCreated("""
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                final webview = await WebviewWindow.create(
+                  configuration: CreateConfiguration(
+                    windowHeight: 1280,
+                    windowWidth: 720,
+                    title: "ExampleTestWindow",
+                    titleBarTopPadding: Platform.isMacOS ? 20 : 0,
+                  ),
+                );
+                webview
+                  ..registerJavaScriptMessageHandler("test", (name, body) {
+                    debugPrint('on javaScipt message: $name $body');
+                  })
+                  ..setApplicationNameForUserAgent(" WebviewExample/1.0.0")
+                  ..setPromptHandler((prompt, defaultText) {
+                    if (prompt == "test") {
+                      return "Hello World!";
+                    } else if (prompt == "init") {
+                      return "initial prompt";
+                    }
+                    return "";
+                  })
+                  ..addScriptToExecuteOnDocumentCreated("""
   const mixinContext = {
     platform: 'Desktop',
     conversation_id: 'conversationId',
@@ -67,39 +87,75 @@ class _WebViewExampleState extends State<WebViewExample> {
     }
   }
 """)
-        ..launch("https://flutter.dev/");
-    } else {
-      _controller = WebViewController()
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setNavigationDelegate(
-          NavigationDelegate(
-            onPageFinished: (String url) async {
-              // 页面加载完成后获取网页内容
-              try {
-                final String? pageContent =
-                    await _controller.runJavaScriptReturningResult(
-                        'document.documentElement.outerHTML;') as String?;
-
-                if (pageContent != null) {
-                  print('网页内容: $pageContent');
-                } else {
-                  print('未能获取网页内容');
-                }
-              } catch (e) {
-                print('获取网页内容时出错: $e');
-              }
-            },
+                  ..launch("http://localhost:3000/test.html");
+              },
+              icon: const Icon(Icons.bug_report),
+            )
+          ],
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(controller: _controller),
+                const SizedBox(height: 16),
+                TextButton(
+                  onPressed: _webviewAvailable != true ? null : _onTap,
+                  child: const Text('Open'),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () async {
+                    debugPrint('clear complete');
+                  },
+                  child: const Text('Clear all'),
+                )
+              ],
+            ),
           ),
-        )
-        ..loadRequest(Uri.parse('https://flutter.dev/'));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Flutter WebView Example')),
-      body: WebViewWidget(controller: _controller),
+        ),
+      ),
     );
   }
+
+  void _onTap() async {
+    final webview = await WebviewWindow.create(
+      configuration: CreateConfiguration(
+        titleBarTopPadding: Platform.isMacOS ? 20 : 0,
+      ),
+    );
+    webview
+      ..setBrightness(Brightness.dark)
+      ..setApplicationNameForUserAgent(" WebviewExample/1.0.0")
+      ..launch(_controller.text)
+      ..onClose.whenComplete(() {
+        debugPrint("on close");
+      });
+    await Future.delayed(const Duration(seconds: 2));
+    for (final javaScript in _javaScriptToEval) {
+      try {
+        final ret = await webview.evaluateJavaScript(javaScript);
+        debugPrint('evaluateJavaScript: $ret');
+      } catch (e) {
+        debugPrint('evaluateJavaScript error: $e \n $javaScript');
+      }
+    }
+  }
 }
+
+const _javaScriptToEval = [
+  """
+  function test() {
+    return;
+  }
+  test();
+  """,
+  'eval({"name": "test", "user_agent": navigator.userAgent})',
+  '1 + 1',
+  'undefined',
+  '1.0 + 1.0',
+  '"test"',
+];
